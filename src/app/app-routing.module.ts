85011415bd2142df05030d4227488ab269703d13
Joshua Samuel-Ojo <oludunsinsamuelojo@chevron.com>
2021-06-06 09:17:41 -0700

    initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HostComponent } from './pages/host/host.component';
+import { ListenerComponent } from './pages/listener/listener.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'host', component: HostComponent },
+  { path: 'listener', component: ListenerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import {ToastrModule} from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HostComponent } from './pages/host/host.component';
+import { ListenerComponent } from './pages/listener/listener.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HostComponent,
+    ListenerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
