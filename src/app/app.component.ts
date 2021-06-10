d91890506e3e2fb0e4536d1cd5fbc579b47671be
Joshua Samuel-Ojo <oludunsinsamuelojo@chevron.com>
2021-06-09 19:21:51 -0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,21 +29,104 @@ export class AppComponent implements OnInit{
   listener = false;
 
   participantsList = [];
-
+  conference: any;
   conferenceStarted = false;
 
-  public conferenceName = 'podville#0001';
-  public name = 'Joshua';
+  public conferenceName = 'podville#3412';
+  public name = '';
   public message = '';
   public inProgress = false;
 
   constructor(
     private snackBar: MatSnackBar
   ) {
+    VoxeetSDK.conference.on('streamAdded', (participant, stream) => {
+      console.log('Stream Added with participant id: ' + participant.id + ' with stream type: ' + this.fetchStreamStype(stream));
+      if (stream.getVideoTracks().length) {
+        this.addVideoNode(participant, stream);
+      }
+      if (stream.type === 'ScreenShare') {
+        //this.addScreenShareNode(stream);
+      }
+    });
 
+    VoxeetSDK.conference.on('streamRemoved', (participant, stream) => {
+      console.log('Stream removed with participant id:' + participant.id + ' with stream type: ' + this.fetchStreamStype(stream));
+      if (stream.getVideoTracks().length) {
+        this.removeVideoNode(participant);
+      }
+      if (stream.getAudioTracks().length) {
+        stream.getAudioTracks().forEach((track) => {
+          track.enabled = false;
+          track.stop();
+          track = null;
+        });
+        this.removeParticipantNode(participant);
+      }
+      if (stream.type === 'ScreenShare') {
+        //this.removeScreenShareNode();
+      }
+    });
+
+    VoxeetSDK.conference.on('streamUpdated', (participant, stream) => {
+      console.log('streamUpdated with participant id: ' + participant.id + ' with stream type: ' + this.fetchStreamStype(stream));
+      this.showAllVideoParticipants();
+      if (!participant.streams[0].getVideoTracks().length) {
+        this.removeVideoNode(participant);
+      }
+    });
+
+    VoxeetSDK.conference.on('participantAdded', (participant, stream) => {
+      console.log('participantAdded with participant id' + participant.id + ' with stream type: ' + this.fetchStreamStype(stream));
+      this.addParticipantNode(participant);
+      this.conferenceStarted = VoxeetSDK.conference.participants.length > 0;
+    });
+
+    VoxeetSDK.conference.on('participantUpdated', (participant, stream) => {
+      console.log('participantUpdated with participant id' + participant.id + ' with stream type: ' + this.fetchStreamStype(stream));
+      if (participant.status === 'Left') {
+        this.removeParticipantNode(participant);
+      }
+    });
+
+    VoxeetSDK.notification.on('conferenceEnded', (participant, stream) => {
+      console.log('Conference has ended' + participant.info.name);
+      this.removeAllParticipantNodes();
+      this.reset();
+    });
+
+    VoxeetSDK.command.on('received', (participant, message) => {
+      alert('New message by ' + participant.info.name + ': ' + message);
+    });
   }
 
   ngOnInit() {}
+
+  // Audio
+  muteYourself() {
+    this.setInProgress(true);
+    if (!this.audio) {
+      VoxeetSDK.conference.startAudio(VoxeetSDK.session.participant)
+        .then(() => {
+          console.log('audio stream started');
+          this.setInProgress(false);
+        }).catch((e) => {
+        this.setInProgress(false);
+        console.log('Error while starting audio' + e);
+      });
+    } else {
+      VoxeetSDK.conference.stopAudio(VoxeetSDK.session.participant)
+        .then(() => {
+          console.log('audio stream stopped');
+          this.setInProgress(false);
+        }).catch((e) => {
+        this.setInProgress(false);
+        console.log('Error while stopping audio' + e);
+      });
+    }
+
+    this.audio = !this.audio;
+  }
 
   // Videos
   startVideo() {
@@ -118,6 +201,55 @@ export class AppComponent implements OnInit{
     videoNode.srcObject = null;
   }
 
+  // Listener
+  joinAsListener(){
+    if (VoxeetSDK.conference.participants.length) {
+      VoxeetSDK.session.close();
+    }
+    if (this.validateUserAndConferenceName()) {
+      this.setInProgress(true);
+      this.createSessionForUser()
+        .then(() => {
+          VoxeetSDK.conference.create({alias: this.conferenceName})
+            .then((conference) => {
+              VoxeetSDK.conference.listen(conference)
+                .then(() => {
+                  console.log('Listening conference');
+                  this.callJoined = true;
+                  this.listener = true;
+                  this.setInProgress(false);
+                })
+                .catch(e => {
+                  console.log('Listening conference failed' + e);
+                  this.setInProgress(false);
+                });
+            });
+        });
+    }
+  }
+  addParticipantNode(participant) {
+    const participantsList = document.getElementById('participants-list');
+
+    if (participantsList) {
+
+      // if the participant is the current session user, don't add himself to the list
+      if (participant.id === VoxeetSDK.session.participant.id) {
+        return;
+      }
+
+      let participantNode = document.getElementById('participant-' + participant.id);
+
+      // If Participant Node does not exists
+      if (!participantNode) {
+        participantNode = document.createElement('li');
+        participantNode.setAttribute('id', 'participant-' + participant.id);
+        participantNode.innerText = `${participant.info.name}`;
+        this.openSnackBar(`${participant.info.name}` + ' has joined the conference');
+
+        participantsList.appendChild(participantNode);
+      }
+    }
+  }
 
 
   // Navigate To Host page and to Listener Page
@@ -135,6 +267,7 @@ export class AppComponent implements OnInit{
   }
   toListener(){
     if(this.name.length > 0){
+      this.joinAsListener()
       this.tabIndex = 2;
     } else {
       this.openSnackBar("Please enter a name");
@@ -173,7 +306,14 @@ export class AppComponent implements OnInit{
         });
     }
   }
+  fetchStreamStype(stream) {
+    if (stream) {
+      return stream.type ? stream.type : 'Audio';
+    }
+    return 'No stream available';
+  }
   leaveSession(){
+    console.log('Leaving session');
     this.setDefaultValue();
     if (VoxeetSDK.conference.participants.size) {
       this.setInProgress(true);
@@ -278,6 +418,20 @@ export class AppComponent implements OnInit{
       horizontalPosition: this.horizontalPosition,
       verticalPosition: this.verticalPosition,
       duration: 800,
+    });
+  }
+  private showAllVideoParticipants() {
+    this.participantsList = VoxeetSDK.conference.participants;
+    this.participantsList.forEach((value, key) => {
+      if (value.id !== VoxeetSDK.session.participant.id
+      ) {
+
+        if (value.streams[0] && value.streams[0].getVideoTracks().length) {
+          this.addVideoNode(value, value.streams[0]);
+          return;
+        }
+
+      }
     });
   }
 
