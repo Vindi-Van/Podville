9cd015e8aa3a031a64c30e74b37a73a650b90e9e
Joshua Samuel-Ojo <oludunsinsamuelojo@chevron.com>
2021-06-10 00:44:30 -0700

    clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,26 +7,21 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HostComponent } from './pages/host/host.component';
-import { ListenerComponent } from './pages/listener/listener.component';
-
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
 @NgModule({
   declarations: [
-    AppComponent,
-    HostComponent,
-    ListenerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
